e7e866d4fb76920a90dded74bb7fcaf23f5ad643
Raghav Narula <git@raghavnarula.co.uk>
2020-12-03 12:50:27 +0000

    used generators to make moves and made solution data driven

--- day3/day3.hs
+++ day3/day3.hs
@@ -1,27 +1,25 @@
+-- Generate a list of possible moves using right(r), down(d), width(x) height(y)
+gatherMoves :: (Integral a) => a -> a -> a -> a -> [(a, a)]
+gatherMoves r d x y = zip [a `rem` x | a <- [r, (r + r)..]] [b | b <- [d, (d + d)..y], b < y]
 
-movesImpl r d y [] = movesImpl r d y [(r, d)]
-movesImpl _ _ y ((_, b):xs)
-    | b >= y = xs
-movesImpl r d y ((a, b):xs) = movesImpl r d y $ (a + r, b + d) : (a, b) : xs
-
-moveLst r d x y = map (\(a, b) -> (a `rem` x, b)) $ movesImpl r d y []
-
+-- Check if a particular cell (x, y) is a tree in the (input)
 isTree :: (Int, Int) -> [[Char]] -> Bool
-isTree (x, y) xs = (xs !! y) !! x == '#'
+isTree (x, y) input = (input !! y) !! x == '#'
 
-trees r d map =
-    let x = length $ head map; y = length map
-        in length [0 | m <- moveLst r d x y, isTree m map]
+-- Count the number of cells which are trees in the (input) by moving in incements of right(r) and down(d)
+countTrees :: [[Char]] -> (Int, Int) -> Int
+countTrees input (r, d)  =
+    let x = length $ head input; y = length input
+        in length [0 | m <- gatherMoves r d x y, isTree m input]
+
+-- Map tree counting given an (input) over a list of (right, down) move increments, then take product
+solution :: [[Char]] -> [(Int, Int)] -> Int
+solution input = product . map (countTrees input)
 
 main :: IO ()
 main = do 
     file <- readFile "input.txt"
-    let map = lines file
-    let trees11 = trees 1 1 map
-    let trees31 = trees 3 1 map
-    let trees51 = trees 5 1 map
-    let trees71 = trees 7 1 map
-    let trees12 = trees 1 2 map
-    putStrLn $ "Part 1: " ++  (show $ trees31)
-    putStrLn $ "Part 2: " ++ (show $ trees11 * trees31 * trees51 * trees71 * trees12)
-    -- putStrLn $ "Test 1: " ++  (show $ (movesImpl 3 1 4 []))
+    let input = lines file
+    putStrLn $ "Part 1: " ++ (show $ solution input [(3, 1)])
+    putStrLn $ "Part 1: " ++ (show $ solution input [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])
+    -- putStrLn $ "Test 1: " ++  (show $ take 100 (gatherMoves 3 1 7 7))
